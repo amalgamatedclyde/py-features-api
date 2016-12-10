aff4898dca86372384c74725dd59f1e245ccb631
amalgamatedclyde <clyde@amalgamated-clyde.com>
2016-12-10 09:11:08 -0500

    cleaned up redundant code

--- nets.py
+++ nets.py
@@ -6,65 +6,11 @@ import  base64
 # Taken from the official Caffe repo
 from app import ImagenetClassifier, ClassificationError
 
-REPO_DIRNAME = "/home/ubuntu/py-features-api"
-
-# Configure Caffe and load the model
-# caffe.set_device(0) forces GPU mode
-caffe.set_mode_cpu()
-
-# Initialize classifier + warm start by forward for allocation
-# _classifier_args = {
-#     'model_def_file': (
-#         'models/deploy.prototxt'),
-#     'pretrained_model_file': (
-#         'models/bvlc_reference_caffenet.caffemodel'),
-#     'mean_file': (
-#         'data/ilsvrc_2012_mean.npy'),
-#     'class_labels_file': (
-#         'data/ilsvrc12/synset_words.txt'),
-#     'bet_file': (
-#         'data/ilsvrc12/imagenet.bet.pickle'),
-#     'image_dim': 256,
-#     'raw_scale': 255.0,
-#     'gpu_mode': True,
-# }
-
-_classifier_args = {
-    'model_def_file': (
-        '{}/models/bvlc_reference_caffenet/deploy.prototxt'.format(REPO_DIRNAME)),
-    'pretrained_model_file': (
-        '{}/models/bvlc_reference_caffenet.caffemodel'.format(REPO_DIRNAME)),
-    'mean_file': (
-        '{}/python/caffe/imagenet/ilsvrc_2012_mean.npy'.format(REPO_DIRNAME)),
-    'class_labels_file': (
-        '{}/data/ilsvrc12/synset_words.txt'.format(REPO_DIRNAME)),
-    'bet_file': (
-        '{}/data/ilsvrc12/imagenet.bet.pickle'.format(REPO_DIRNAME)),
-    'image_dim': 256,
-    'raw_scale': 255.0,
-    'gpu_mode': False,
-}
-
-
-_classifier = ImagenetClassifier(**_classifier_args)
+_classifier = ImagenetClassifier()
 _classifier.net.forward()
 
 
 def classify(image_file=None, url=None):
-    """
-    Given something to classify, load the image and then pass it through the
-    network to see what classes we get.
-
-    :param str url: 
-        The URL to a resource for which features should be extracted
-        base64 image_file
-    :returns list: The list of classification results
-
-    """
-    # print('image: {}'.format(image_file))
-
-    # Load the image data from a string buffer
-
     if url:
         string_buffer = StringIO.StringIO(urllib.urlopen(url).read())
         image = caffe.io.load_image(string_buffer)
