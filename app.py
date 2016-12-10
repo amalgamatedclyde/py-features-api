efa6bb10ef0876a5da36f398b37b66523897a99b
amalgamatedclyde <clyde@amalgamated-clyde.com>
2016-12-09 19:31:39 -0500

    path change

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import caffe
 
 
 # REPO_DIRNAME = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/../..')
-REPO_DIRNAME = os.getcwd()
+# REPO_DIRNAME = os.getcwd()
+REPO_DIRNAME = "/home/clyde/markable/py-features-api"
 UPLOAD_FOLDER = '/tmp/caffe_demos_uploads'
 ALLOWED_IMAGE_EXTENSIONS = set(['png', 'bmp', 'jpg', 'jpe', 'jpeg', 'gif'])
 
